3b997442adea63d0e923d1045479be0cebf36604
Morax997 <xhoi.ibra@gmail.com>
2024-03-12 17:34:30 +0100

    implemented option state

--- src/components/PollUnit/PollUnit.jsx
+++ src/components/PollUnit/PollUnit.jsx
@@ -1,31 +1,58 @@
 import "./PollUnit.css";
+import {useState} from "react";
+
+const data = [
+  {
+    id: 1,
+    title: "What is your favorite season?",
+    options: [
+      {
+        id: 1,
+        label: "Spring",
+      },
+      {
+        id: 2,
+        label: "Summer",
+      },
+      {
+        id: 3,
+        label: "Fall",
+      },
+      {
+        id: 4,
+        label: "Winter",
+      },
+    ],
+  },
+];
 
 const PollUnit = () => {
+  const [selectedOptionId, setSelectedOptionId] = useState(null);
+
   return <div className="pollUnit">
     <div className="pollHeader">
       Vote Below
     </div>
     <div className="pollContainer">
       <div className="pollTitle">
-        What is your favorite season?
+        {data[0].title}
       </div>
       <div className="pollBars">
-        <div className="pollOption">
-          <input type="radio" checked={true}/>
-          Spring
-        </div>
-        <div className="pollOption">
-          <input type="radio" checked={true}/>
-          Summer
-        </div>
-        <div className="pollOption">
-          <input type="radio" checked={true}/>
-          Fall
-        </div>
-        <div className="pollOption">
-          <input type="radio" checked={true}/>
-          Winter
-        </div>
+        {
+          data[0].options.map(option => {
+            return <div
+              key={option.id}
+              className="pollOption"
+            >
+              <input
+                type="radio"
+                checked={selectedOptionId === option.id}
+                onClick={() => {setSelectedOptionId(option.id)}}
+              />
+              {option.label}
+            </div>
+          })
+        }
       </div>
     </div>
     <button>Vote!</button>
