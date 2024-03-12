ab7ce573f16224a72692c41a4183ae8920da5778
Morax997 <xhoi.ibra@gmail.com>
2024-03-12 18:16:45 +0100

    implemented buttons functionality

--- src/components/PollUnit/PollUnit.jsx
+++ src/components/PollUnit/PollUnit.jsx
@@ -29,9 +29,13 @@ const data = [
 const PollUnit = () => {
   const [selectedOptionId, setSelectedOptionId] = useState(null);
 
+  const clearOption = () => {
+    setSelectedOptionId(null);
+  }
+
   return <div className="pollUnit">
     <div className="pollHeader">
-      Vote Below
+      Vote Below!
     </div>
     <div className="pollContainer">
       <div className="pollTitle">
@@ -55,7 +59,10 @@ const PollUnit = () => {
         }
       </div>
     </div>
-    <button>Vote!</button>
+    <div className="buttonsContainer">
+    <button onClick={() => {alert("You Voted " + selectedOptionId)}} className="voteButton">Vote!</button>
+    <button onClick={clearOption} className="clearButton">Clear Selection.</button>
+    </div>
   </div>
 }
 
